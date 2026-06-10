cd937bd08cdb861668fccf0703ac88719e2ca805
Krishna <2492381@cognizant.com>
2026-06-10 14:14:28 +0530

    Create Order table

---
+++ src/main/java/com/cafetron/order/Order.java
@@ -0,0 +1,54 @@
+package com.cafetron.order;
+
+import com.cafetron.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Order")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "overall_status")
+    private String overallStatus;
+
+    @Column(name = "payment_status")
+    private String paymentStatus;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "total_amount")
+    private BigDecimal totalAmount;
+
+    @Column(name = "vendor_count")
+    private Integer vendorCount;
+
+    @Column(name = "vendor_accepted_count")
+    private Integer vendorAcceptedCount;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "ready_at")
+    private LocalDateTime readyAt;
+
+}
