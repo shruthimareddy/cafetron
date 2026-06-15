f930fca9648c655db08b65863af6caacd54e94fd
Krishna <2492381@cognizant.com>
2026-06-15 21:20:11 +0530

    chore: move UserRepository from auth to Repository/User

--- src/main/java/com/cafetron/user/repository/UserRepository.java
+++ src/main/java/com/cafetron/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
-package com.cafetron.auth.repository;
+package com.cafetron.user.repository;
 
 import com.cafetron.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
